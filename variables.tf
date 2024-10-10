3ac3ebfb6d1a159c4fc3418283715efcd0bfe6c5
Everramos0428 <155467583+Everramos0428@users.noreply.github.com>
2024-10-10 04:49:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
+# Nombre del proyecto que se utilizará para nombrar varios recursos en la infraestructura.
 variable "project_name" {
-  type = string
-  description = "Nombre del proyecto"
+  type        = string
+  description = "Nombre único asignado al proyecto, utilizado para identificar y nombrar los recursos dentro de Azure."
 }
 
+# Versión específica de Kubernetes que se utilizará para desplegar el clúster en Azure.
 variable "kubernetes_version" {
-  type = string
-  description = "Version de kubernet usada"
+  type        = string
+  description = "La versión exacta de Kubernetes que se usará para configurar el clúster AKS (Azure Kubernetes Service)."
 }
 
+# Región o ubicación geográfica en la que se desplegarán los recursos dentro de Azure.
 variable "location" {
-  type = string
-  description = "Ubicacion geografica de los recursos"
+  type        = string
+  description = "La ubicación geográfica o región en la que se desplegarán los recursos de Azure, como el clúster de Kubernetes, el registro de contenedores y otros servicios."
 }
 
+# Cantidad de nodos que se desplegarán en el clúster de Kubernetes.
 variable "node_count" {
-  type = number
-  description = "Numero de nodos del cluster de kubernet"
+  type        = number
+  description = "Número de nodos de agente que se crearán en el grupo de nodos del clúster de Kubernetes."
 }
 
+# Nombre del dominio de prueba personalizado que se utilizará para las pruebas de configuración del DNS.
 variable "dominio" {
-  type = string
-  description = "Dominio personalizado de prueba"
+  type        = string
+  description = "Dominio personalizado que se utilizará para configurar la zona DNS dentro de Azure como parte del entorno de pruebas."
 }
