6832b2cfecb9c316f7d6b3a06f091004b65644c5
Everramos0428 <155467583+Everramos0428@users.noreply.github.com>
2024-10-10 04:42:22 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,19 @@
+# Muestra el nombre del grupo de recursos creado.
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
+# Muestra la dirección IP pública que se ha creado.
 output "public_ip_address" {
   value = azurerm_public_ip.public_ip.ip_address
 }
 
+# Muestra los servidores de nombres de la zona DNS creada.
 output "dns" {
   value = azurerm_dns_zone.dns.name_servers
 }
 
+# Muestra el nombre del dominio asociado a la zona DNS.
 output "dominio" {
   value = azurerm_dns_zone.dns.name
 }
